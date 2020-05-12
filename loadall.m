1bffd52ca3f28df9ee9821d685552cbad83c4d07
amj-git <58462676+amj-git@users.noreply.github.com>
2020-05-12 20:21:44 +0100

    comments added

--- loadall.m
+++ loadall.m
@@ -1,7 +1,8 @@
 
 
 %Centre on a reference file
-
+%Edit this line to choose which file to reference your initial map on
+%You can zoom around after this
 ref=loadgpx("data\activities\1289707912.gpx");
 minx=min(ref(:,5));
 maxx=max(ref(:,5));
@@ -12,6 +13,7 @@ maxy=max(ref(:,4));
 deltax=maxx-minx;
 deltay=maxy-miny;
 
+%choose a scale factor for the initial map
 scale=1.5;
 minx=minx-deltax*(scale-1);
 maxx=maxx+deltax*(scale-1);
@@ -26,6 +28,8 @@ ylim([miny maxy])
 axis equal;
 grid on;
 hold on;
+
+%Edit this to point to a folder full of your GPX files
 myDir = "data\activities\";
 myFiles = dir(fullfile(myDir,'*.gpx'));
 
@@ -35,18 +39,20 @@ for k = 1:length(myFiles)
   fullFileName = fullfile(myDir, baseFileName);
   %fprintf(1, 'Now reading %s\n', fullFileName);
   
+  %use loadgpx to import the file
   coords{k}=loadgpx(fullFileName);
   fnames{k}=fullFileName;
   p(k)=plot(coords{k}(:,5),coords{k}(:,4)); hold on;
-  p(k).LineWidth=2;
-  p(k).Color=[1 0 0 0.1];
+  p(k).LineWidth=2;  %2 pixel width - you can change this
+  p(k).Color=[1 0 0 0.1];  %we use an alpha of 0.1 - you can change this and the colour
   xlim([minx maxx]);
   ylim([miny maxy]); 
-  title(k);
+  title(k);  %title displays the number of files processed
   drawnow;
 end
 
+%Add the google maps background using a function from another project
 plot_google_map;
 
-%animate
+%animate (if you like) - you could take a vid of this
 set(p,'Visible','off'); for i=1:numel(p);set(p(i),'Visible','on');drawnow;end;
